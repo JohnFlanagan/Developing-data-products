bb184f6119e1d848f6bb4aa460578ab5deb77741
John Flanagan <laoisman@gmail.com>
2014-09-18 12:19:19 +0200

    Developing data products app
    server.R, ui.R and documentation for developing data products app

---
+++ server.R
@@ -0,0 +1,12 @@
+happinessfnc <- function(id1, id2) {
+        id2 <- as.numeric(id2)
+        round(id1^3 * id2^6 * runif(1, 999,1011),0)
+}
+
+shinyServer(
+        function(input, output) {
+                output$happyvalue <- renderPrint({input$id1})
+                output$odate <- renderPrint({input$date})
+                output$happyscore <- renderPrint({happinessfnc(input$id1, input$id2)})
+        }
+)
---
+++ ui.R
@@ -0,0 +1,23 @@
+library(shiny)
+shinyUI(pageWithSidebar(
+        headerPanel("Happiness indicator"),
+        sidebarPanel(
+                numericInput('id1', 'On a scale of 1 - 10, how happy do you feel today?', 
+                             0, min = 0, max = 10, step = 1),
+                radioButtons("id2", "Is/was the sun shining today?",
+                                   c("Not a lot" = "1",
+                                     "A little" = "2",
+                                     "From dawn to dusk!" = "3")),
+                dateInput("date", "Date:")  
+        ),
+        mainPanel(
+                h3('How happy are you today'),
+                h4('You said that on a scale of 1 - 10, you felt happiness on a scale of'),
+                verbatimTextOutput("happyvalue"),
+                h4('today, the'),
+                verbatimTextOutput("odate"),
+                h4('Based on this (and on the smile on your face!), 
+                   your happiness score for today is'),
+                verbatimTextOutput("happyscore")
+        )
+))
